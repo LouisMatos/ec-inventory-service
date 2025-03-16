fee6c04a419ce966c4a350e305d365ecf76f6d26
LouisMatos <luismatos000@gmail.com>
2025-03-16 19:58:44 -0300

    first commit

---
+++ src/test/java/br/matosit/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.matosit.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
